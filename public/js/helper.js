66193547e2bdcf80ad61fa28bd7914f431f90bf0
Azhar Malik <azharmalik02@gmail.com>
2019-01-01 07:24:53 +0100

    Made some changes according to evercam requirements

--- public/js/helper.js
+++ public/js/helper.js
@@ -5,11 +5,28 @@ function secondsToHuman(seconds)
     var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
     var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
     var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
-    return numyears + " years " +  numdays + " days " + numhours + " hours " + numminutes + " minutes " + numseconds.toFixed() + " seconds";
+    var time_str = numminutes + " minutes " + numseconds.toFixed() + " seconds";
+    if (numhours > 0) {
+      time_str = numhours + " hours " + time_str
+    }
+    
+    if (numdays > 0) {
+      time_str = numdays + " days " + time_str
+    }
+
+    if (numyears > 0) {
+      time_str = numyears + " years " +  time_str
+    }
+
+    return time_str
 }
 
 function byteToHuman(bytes) {
+  if (bytes == "0" || bytes == "") {
+    return '0 bytes'
+  } else {
     var s = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
     var e = Math.floor(Math.log(bytes) / Math.log(1024));
     return (bytes / Math.pow(1024, e)).toFixed(2) + " " + s[e];
+  }
 }
--- public/js/model.js
+++ public/js/model.js
@@ -49,16 +49,30 @@ function setLiveStream(data)
     var table = "";
     var language_play = $("#language_play").val();
     var language_drop = $("#language_drop").val();
+    var exids = "";
     $.each(data, function (index,channel) {
 
-
-        var name = "<td class='text-center'>"+channel.name[0]+"</td>";
+	var hls = 0;
+        var rtmp = 0;
+        if (channel.nclients[0] > 0) {
+            hls = 1;
+            rtmp = channel.nclients[0] - hls;
+        }
+	var decode_token = Base64.decode(channel.name[0]);
+        var camera_name = decode_token.split("|")[1];
+	if (index == 0) {
+		exids = decode_token.split("|")[0];
+	} else {
+		exids = exids + "," + decode_token.split("|")[0];
+	}
+        var name = "<td class='text-center'style='overflow: hidden;position: relative;white-space: nowrap;box-sizing:border-box;display:block;max-width:150px;'>"+camera_name+"</td>";
         var bw_in = "<td class='text-center'>"+byteToHuman(channel.bw_in[0])+"</td>";
         var bw_out = "<td class='text-center'>"+byteToHuman(channel.bw_out[0])+"</td>";
         var bytes_in = "<td class='text-center'>"+byteToHuman(channel.bytes_in[0])+"</td>";
         var bytes_out = "<td class='text-center'>"+byteToHuman(channel.bytes_out[0])+"</td>";
         var time  = "<td class='text-center'>"+secondsToHuman(channel.time[0] / 1000)+"</td>";
-        var viewers = "<td class='text-center'><b>"+numeral(channel.nclients[0]).format('0,0')+"<b/></td>";
+        var viewers_rtmp = "<td class='text-center'><b>"+numeral(rtmp).format('0,0')+"<b/></td>";
+	var viewers_hls = "<td class='text-center'><b>"+numeral(hls).format('0,0')+"<b/></td>";
 	var v_w = 0;
 	var v_h = 0;
 	if (typeof channel.meta !== 'undefined') {
@@ -68,11 +82,11 @@ function setLiveStream(data)
         var resolution = "<td class='text-center'>"+ v_w + " X " + v_h+"</td>";
         var play = "<td><button class='btn btn-default play_stream' data-stream-name='"+channel.name[0]+"' data-toggle='modal' data-target='#stream_popup'><i class='glyphicon glyphicon-play'></i> "+language_play+"</button></td>";
 	    var drop = "<td><button class='btn btn-default drop_stream' data-stream-name='"+channel.name[0]+"'><i class='glyphicon glyphicon-stop'></i> "+language_drop+"</button></td>";
-        table = table + "<tr>"+name+resolution+bw_in+bw_out+bytes_in+bytes_out+viewers+time+play+drop+"</tr>";
+        table = table + "<tr>"+name+resolution+bw_in+bw_out+bytes_in+bytes_out+viewers_rtmp+viewers_hls+time+drop+"</tr>";
 
     });
 
-
+    $("#camera_exids").val(exids);
     $("#live_stream").html(table);
 
     playStreamEvent();
